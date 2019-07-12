d15be3a1865869bf97052fd8b55f43f5ed98d5f5
chris <497960835@qq.com>
2019-07-12 17:58:39 +0800

    13.罗马数字转整数

---
+++ 00013.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function romanToInt($s) {
+        $char_arr = ['I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000];
+
+        # 遍历字符串
+        $sum = 0;
+        $stack = 0;
+        for($i=0; $i<strlen($s); $i++) {
+            $char = strtoupper($s[$i]);
+            # 判断当前字符组合标志位是否符合特殊情况
+            if($stack>0) {
+                if(in_array(intval($char_arr[$char]/$stack), [5,10], true)) { # 特殊情况
+                    $sum += ($char_arr[$char]-$stack);
+                    $stack = 0;
+                    continue;
+                }
+                # 非特殊情况，加上标志位
+                $sum += $stack;
+            }
+            # 判断当前字符是否符合标志位
+            if(in_array($char, ['I','X','C'], true)) { # 可能满足特殊情况的，先将字符的值存入标志位，待下一步确认；
+                $stack = $char_arr[$char];
+                continue;
+            } else {
+                $stack = 0; # 恢复标志位
+            }
+
+            $sum += $char_arr[$char]; # 正常求和
+        }
+
+        return $sum+$stack;
+    }
+}
